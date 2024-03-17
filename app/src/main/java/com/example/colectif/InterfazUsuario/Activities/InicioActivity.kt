e49a076ac28360378e6e8286b3df81ca3574415d
SrCastaman <rafacastaman29@gmail.com>
2024-03-17 20:41:12 +0100

    content main y toolbar colocado

--- app/src/main/java/com/example/colectif/InterfazUsuario/Activities/InicioActivity.kt
+++ app/src/main/java/com/example/colectif/InterfazUsuario/Activities/InicioActivity.kt
@@ -2,6 +2,10 @@ package com.example.colectif.InterfazUsuario.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.colectif.R
 import com.example.colectif.databinding.ActivityInicioBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -10,35 +14,16 @@ import com.google.firebase.database.ValueEventListener
 
 class InicioActivity : AppCompatActivity() {
     private lateinit var binding: ActivityInicioBinding
-    private lateinit var idUser: String
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInicioBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        idUser = intent.getStringExtra("id").toString()
+        setSupportActionBar(binding.toolbar)
 
-
-
-        var database = FirebaseDatabase.getInstance("https://colectif-project-default-rtdb.europe-west1.firebasedatabase.app/")
-        var ref = database.getReference("users")
-
-
-
-        ref.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                binding.txtNombre.text = snapshot.child(idUser).child("name").value.toString()
-                binding.txtApellido.text = snapshot.child(idUser).child("firstSurName").value.toString()
-                binding.txtUsuario.text = snapshot.child(idUser).child("userName").value.toString()
-                binding.txtCorreo.text = snapshot.child(idUser).child("mail").value.toString()
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-            }
-
-        })
-
-
-
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
 
     }
