359cab199ccdc06b809c002a149e93624c4a06a5
Alejandra Rodríguez <124720664+ARP-10@users.noreply.github.com>
2024-03-17 18:58:44 +0100

    Delete AndroidStudioProjects/PMDM/Colectif-master directory

--- AndroidStudioProjects/PMDM/Colectif-master/app/src/main/java/com/example/colectif/models/Grupo.kt
+++
@@ -1,5 +0,0 @@
-package com.example.colectif.models
-
-data class Grupo (val id: Int, val nombre: String, val administrador: String, val categoria: String, val imagen: String) {
-
-}
--- AndroidStudioProjects/PMDM/Colectif-master/app/src/main/java/com/example/colectif/models/User.kt
+++
@@ -1,4 +0,0 @@
-package com.example.colectif.models
-
-data class User(val name : String, val firstSurName : String, val secondSurName : String
-                , val mail : String, val userName : String)
