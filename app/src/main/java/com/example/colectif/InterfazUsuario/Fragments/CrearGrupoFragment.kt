e44521a46855761cade5d8e37144277f5dad7d43
Alejandra Rodríguez <124720664+ARP-10@users.noreply.github.com>
2024-03-20 00:07:38 +0100

    Update CrearGrupoFragment.kt

--- app/src/main/java/com/example/colectif/InterfazUsuario/Fragments/CrearGrupoFragment.kt
+++ app/src/main/java/com/example/colectif/InterfazUsuario/Fragments/CrearGrupoFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.colectif.R
 import com.example.colectif.databinding.FragmentCrearGrupoBinding
@@ -41,11 +42,11 @@ class CrearGrupoFragment: Fragment() {
         // Netflix marcado por defecto
         binding.spinnerApps.setSelection(0)
 
-        // Comprobar si todos los campos están rellenos
+        // TODO: Comprobar si todos los campos están rellenos
         binding.btnCrearGrupo.setOnClickListener {
-            nombreGrupo = binding.editNombre.text.toString()
-            plan = binding.editPlan.text.toString()
-            precio = binding.editPrecio.text.toString().toDouble()
+            //nombreGrupo = binding.editNombre.text.toString()
+            //plan = binding.editPlan.text.toString()
+            //precio = binding.editPrecio.text.toString().toDouble()
             if (nombreGrupo != "" || plan != "" || precio == 0.0) {
                 agregarGrupo(nombreGrupo, plan, precio)
             } else {
@@ -57,11 +58,85 @@ class CrearGrupoFragment: Fragment() {
         binding.spinnerApps.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
 
+                // Comprobamos que app ha elegido
                 when (position) {
-                    0 -> binding.imgGrupo.setImageResource(R.drawable.netflix)
-                    1 -> binding.imgGrupo.setImageResource(R.drawable.spotify)
-                    2 -> binding.imgGrupo.setImageResource(R.drawable.amazon)
-                    3 -> binding.imgGrupo.setImageResource(R.drawable.disney)
+                    0 -> {
+                        binding.imgGrupo.setImageResource(R.drawable.netflix)
+                        // Buscamos el string-array correspondiente
+                        val adapter = ArrayAdapter.createFromResource(requireActivity(), R.array.plan_netflix, android.R.layout.simple_spinner_item)
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                        binding.spinnerPlan.adapter = adapter
+                        // Manejamos el spinner en base a ese string-array
+                        binding.spinnerPlan.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                                when (position) {
+                                    0 -> binding.txtPrecioCorrecto.text = "5.49 €/mes"
+                                    1 -> binding.txtPrecioCorrecto.text = "12.99 €/mes"
+                                    2 -> binding.txtPrecioCorrecto.text = "17.99 €/mes"
+                                }
+                            }
+                            override fun onNothingSelected(parent: AdapterView<*>?) {
+                                TODO("Not yet implemented")
+                            }
+                        }
+                    }
+                    1 -> {
+                        binding.imgGrupo.setImageResource(R.drawable.spotify)
+                        val adapter = ArrayAdapter.createFromResource(requireActivity(), R.array.plan_sopitfy, android.R.layout.simple_spinner_item)
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                        binding.spinnerPlan.adapter = adapter
+                        binding.spinnerPlan.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                                when (position) {
+                                    0 -> binding.txtPrecioCorrecto.text = "10.99 €/mes"
+                                    1 -> binding.txtPrecioCorrecto.text = "5.99 €/mes"
+                                    2 -> binding.txtPrecioCorrecto.text = "14.99 €/mes"
+                                    3 -> binding.txtPrecioCorrecto.text = "17.99 €/mes"
+                                }
+                            }
+
+                            override fun onNothingSelected(parent: AdapterView<*>?) {
+                                TODO("Not yet implemented")
+                            }
+                        }
+                    }
+                    2 -> {
+                        binding.imgGrupo.setImageResource(R.drawable.amazon)
+                        val adapter = ArrayAdapter.createFromResource(requireActivity(), R.array.plan_amazon, android.R.layout.simple_spinner_item)
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                        binding.spinnerPlan.adapter = adapter
+                        binding.spinnerPlan.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                                when (position) {
+                                    0 -> binding.txtPrecioCorrecto.text = "4.99 €/mes"
+                                    1 -> binding.txtPrecioCorrecto.text = "2.49 €/mes"
+                                }
+                            }
+
+                            override fun onNothingSelected(parent: AdapterView<*>?) {
+                                TODO("Not yet implemented")
+                            }
+                        }
+                    }
+                    3 -> {
+                        binding.imgGrupo.setImageResource(R.drawable.disney)
+                        val adapter = ArrayAdapter.createFromResource(requireActivity(), R.array.plan_disney, android.R.layout.simple_spinner_item)
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                        binding.spinnerPlan.adapter = adapter
+                        binding.spinnerPlan.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                                when (position) {
+                                    0 -> binding.txtPrecioCorrecto.text = "5.99 €/mes"
+                                    1 -> binding.txtPrecioCorrecto.text = "8.99 €/mes"
+                                    2 -> binding.txtPrecioCorrecto.text = "11.99 €/mes"
+                                }
+                            }
+
+                            override fun onNothingSelected(parent: AdapterView<*>?) {
+                                TODO("Not yet implemented")
+                            }
+                        }
+                    }
                 }
             }
 
@@ -70,9 +145,9 @@ class CrearGrupoFragment: Fragment() {
             }
         }
 
-
     }
 
+    // TODO: COMPROBAR SI FUNCIONA
     private fun agregarGrupo(nombre: String, plan: String, precio: Double) {
         val ref = database.getReference("groups")
         val key = ref.push().key ?: ""
