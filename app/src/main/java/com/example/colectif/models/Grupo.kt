bcd1630852534e8779c5ee694c45555e68a919ec
Alejandra Rodríguez <124720664+ARP-10@users.noreply.github.com>
2024-03-25 19:38:41 +0100

    Actualizacion BBDD fragment crear grupo

--- app/src/main/java/com/example/colectif/models/Grupo.kt
+++ app/src/main/java/com/example/colectif/models/Grupo.kt
@@ -1,7 +1,5 @@
 package com.example.colectif.models
 
-import androidx.annotation.DrawableRes
-
-data class Grupo (var nombre: String, var administrador: String, var categoria: String, var plan: String, var precio: Double, @DrawableRes var imagen: Int) {
-    //  , var id: Int,
+data class Grupo (var nombre: String, var app: String, var plan: String, var precio: String, var email: String,var contrasenia: String) {
+    // var imagen: String , var id: Int, var administrador: String
 }
