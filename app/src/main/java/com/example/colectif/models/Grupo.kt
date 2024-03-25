7e2228164574f06db6f66f982997377cf9de3ffb
Alejandra <alejandrarodriguezp97@gmail.com>
2024-03-25 20:18:20 +0100

    Actualización fragment crear grupo: envío de datos a BBDD

--- app/src/main/java/com/example/colectif/models/Grupo.kt
+++ app/src/main/java/com/example/colectif/models/Grupo.kt
@@ -1,5 +1,5 @@
 package com.example.colectif.models
 
 data class Grupo (var administrador: String, var nombre: String, var app: String, var plan: String, var precio: String, var email: String,var contrasenia: String, var imagen: String) {
-
+//pendiente gestionar admin
 }
